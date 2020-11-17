012fa16dc71ee3ff58a57e87d456f0dd547243ba
iyanrspian <iyanrispian@outlook.com>
2020-11-17 14:29:03 +0700

    add marker map on create space

--- public/js/here.js
+++ public/js/here.js
@@ -28,6 +28,75 @@ if (navigator.geolocation) {
         let ui = H.ui.UI.createDefault(map, defaulLayers);
         let mapEvents = new H.mapevents.MapEvents(map);
         let behavior = new H.mapevents.Behavior(mapEvents);
+
+        // Draggable Marker Function
+        function addDraggableMarker(map, behavior){
+            let inputLat = document.getElementById('lat');
+            let inputLng = document.getElementById('lng');
+            
+            if (inputLat.value != '' && inputLng != '') {
+                objLocalCoord = {
+                    lat: inputLat.value,
+                    lng: inputLng.value
+                }
+            }
+
+            let marker = new H.map.Marker(objLocalCoord, {
+                volatility: true
+            })
+
+            marker.draggable = true;
+            map.addObject(marker);
+
+            // disable the default draggability of the underlying map
+            // and calculate the offset between mouse and target's position
+            // when starting to drag a marker object;
+            map.addEventListener('dragstart', function(ev) {
+                let target = ev.target,
+                    pointer = ev.currentPointer;
+                if (target instanceof H.map.Marker) {
+                    let targetPosition = map.geoToScreen(target.getGeometry());
+                    target['offset'] = new H.math.Point(
+                        pointer.viewportX - targetPosition.x, pointer.viewportY - targetPosition.y
+                    );
+                    behavior.disable();
+                }
+            }, false);
+
+            // listen to the drag event and move the position of the marker
+            // as necessary
+            map.addEventListener('drag', function(ev) {
+                let target = ev.target,
+                    pointer = ev.currentPointer;
+                if (target instanceof H.map.Marker) {
+                    target.setGeometry(
+                        map.screenToGeo(
+                            pointer.viewportX - target['offset'].x, pointer.viewportY - target['offset'].y
+                        )
+                    );
+                }
+            }, false);
+
+            // re-enable the default draggability of the underlying map
+            // when dragging has completed
+            map.addEventListener('dragend', function(ev) {
+                let target = ev.target;
+                if (target instanceof H.map.Marker) {
+                    behavior.enable();
+                    let resultCoord = map.screenToGeo(
+                        ev.currentPointer.viewportX,
+                        ev.currentPointer.viewportY
+                    );
+                    // console.log(resultCoord);
+                    inputLat.value = resultCoord.lat;
+                    inputLng.value = resultCoord.lng;
+                }
+            }, false);
+        }
+
+        if (window.action == "submit") {
+            addDraggableMarker(map, behavior);
+        }
     })
 } else {
     console.error("Geolocation isn't supported")
--- resources/views/pages/space/create.blade.php
+++ resources/views/pages/space/create.blade.php
@@ -53,7 +53,7 @@
                     <div class="form-group">
                         <label for="">Latitude</label>
                         {!! Form::text('latitude', null, ['class' => $errors->has('latitude') ? 
-                        'form-control is-invalid' : 'form-control']) !!}
+                        'form-control is-invalid' : 'form-control', 'id' => 'lat']) !!}
                         @error('latitude')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -63,7 +63,7 @@
                     <div class="form-group">
                         <label for="">Longitude</label>
                         {!! Form::text('longitude', null, ['class' => $errors->has('longitude') ? 
-                        'form-control is-invalid' : 'form-control']) !!}
+                        'form-control is-invalid' : 'form-control', 'id' => 'lng']) !!}
                         @error('longitude')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -78,3 +78,9 @@
     </div>
 </div>
 @endsection
+
+@push('script')
+    <script>
+        window.action = "submit"
+    </script>
+@endpush
